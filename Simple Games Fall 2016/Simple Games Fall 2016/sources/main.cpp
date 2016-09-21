dc2b168e57ef4e6094057ae162ae91567b8caac9
MYE1102 <MYE1102>
2016-09-20 23:09:00 -0700

    Initial Commit

---
+++ Simple Games Fall 2016/Simple Games Fall 2016/sources/main.cpp
@@ -0,0 +1,38 @@
+// Simple Game Fall 2016.cpp
+// Day 1
+
+#include <SFML/Graphics.hpp>
+//#include "ResourcePath.h"
+//#include "Overlap.h"
+
+
+int main()
+{
+	//Create the main window
+	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Works!");
+
+	// declare variables
+
+	//start the game loop
+	while (window.isOpen())
+	{
+		//process events
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			//close when the window is closed
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		//clear the screen
+		window.clear();
+		//draw
+
+		//update the window
+		window.display();
+
+	}
+
+	return 0;
+}
